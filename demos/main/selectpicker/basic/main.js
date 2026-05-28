fe93db3c1723020a0d2db4b3008274f513ff6ed6
Sergio Belmar <sbelmar@gmail.com>
2026-05-28 13:05:04 -0400

    chore: main update [1305]

--- demos/main/selectpicker/basic/main.js
+++ demos/main/selectpicker/basic/main.js
@@ -2,14 +2,21 @@ const init = () => {
 	const output = document.body.querySelector(".my-output");
 	const selectpicker = new WUISelectpicker({
 		selector: ".wui-selectpicker.my-selectpicker",
+		//options: [],
 		value: "2",
 		//lang: "en",
 		//texts: {},
 		//openDirection: "down",
+		//boxAlign: "center",
+		//required: true,
+		//hidden: false,
+		//autochange: true,
 		//multiple: false,
 		//separatorValue: ",",
 		//separatorText: ", ",
 		//filterable: true,
+		//viewicon: false,
+		//viewtext: true,
 		//enabled: true,
 		onOpen: (value) => {
 			output.textContent = `Opened - value: ${value}`;
